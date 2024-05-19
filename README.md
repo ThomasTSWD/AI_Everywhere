0b491e94f88b16f47ba293f7f7833339b94576c5
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-05-19 21:02:41 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# AI Everywhere
+
+![AI Everywhere](screenshot.JPG)
+
+The Intelligent Assistant app is a desktop application with several built-in AI features, including ChatGPT, Mistral AI, Otio AI, Perplexity and Claude AI. It offers a chat interface for interacting with AIs. Views are responsive and adapt to window size. The application features an intuitive main menu, and windows always remain in the foreground. Conversations and states are preserved when tabs are changed.
+
+**Using AI Fast !**
+
+## Usage
+
+### INSTALL
+
+> npm install --save-dev electron
+
+> ( npm install --save-dev electron@latest )
+
+### TEST
+
+> npm start
+
+
+### DEPLOY
+
+#### PC
+
+> npx electron-packager . NomDeVotreApp --platform=win32 --arch=x64 --overwrite
+
+> npx electron-packager . AI_Everywhere --platform=win32 --arch=x64 --icon=assets/icon.ico --overwrite
+
+
+#### MAC 
+
+> npx electron-packager . AI_Everywhere --platform=darwin --arch=x64 --overwrite --icon=assets/icon.icns
+
+
+----
+
+
+## Ressources 
+
+ChatGPT Web
+
+https://github.com/Niek/chatgpt-web
+
+
+Electron.js
+
+https://www.electronjs.org/fr/
+
---
+++ main.js
@@ -0,0 +1,149 @@
+const { app, BrowserWindow, globalShortcut, Menu, screen, BrowserView } = require('electron');
+
+let mainWindow;
+let isFollowingCursor = true;
+
+
+const views = {};
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 700,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            sandbox: true,
+            ignoreCertificateErrors: true,
+            alwaysOnTop: true,
+        },
+        icon: 'assets/icon.png'
+    });
+
+    // Création des BrowserView pour chaque page
+    views['webGpt'] = new BrowserView();
+    views['chatGpt'] = new BrowserView();
+    views['Mistral'] = new BrowserView();
+    views['Claude'] = new BrowserView();
+    views['Otio'] = new BrowserView();
+    views['Perplexity'] = new BrowserView();
+    views['Huggingface'] = new BrowserView();
+
+    // Charger les URL dans les vues respectives
+    views['webGpt'].webContents.loadURL(`file://${__dirname}/gpt/index.html`);
+    views['chatGpt'].webContents.loadURL(`https://chat.openai.com/`);
+    views['Mistral'].webContents.loadURL(`https://chat.mistral.ai/`);
+    views['Claude'].webContents.loadURL(`https://claude.ai/`);
+    views['Otio'].webContents.loadURL(`https://app.otio.ai/`);
+    views['Perplexity'].webContents.loadURL(`https://www.perplexity.ai/collections/Rdaction-Email-QezJEyVWRUG8oV_rsz_4Yg`);
+    views['Huggingface'].webContents.loadURL(`https://huggingface.co/chat/`);
+
+
+    const menuOptions = [
+        { label: 'ChatGPT (3.5)', view: 'chatGpt' },
+        { label: 'WebGPT (API)', view: 'webGpt' },
+        { label: 'Mistral (FR)', view: 'Mistral' },
+        { label: 'Claude (PDF, IMG)', view: 'Claude' },
+        { label: 'Huggingface (GPT\'S)', view: 'Huggingface' },
+        { label: 'Otio (URL)', view: 'Otio' },
+        { label: 'Perplexity (Mail, Search)', view: 'Perplexity' },
+    ];
+
+
+
+    mainWindow.setBrowserView(views['chatGpt']);
+    views['chatGpt'].setBounds({ x: 0, y: 0, width: 700, height: 800 });
+    mainWindow.on('resize', () => {
+        const { width, height } = mainWindow.getContentBounds();
+        Object.values(views).forEach(view => {
+            view.setBounds({ x: 0, y: 0, width, height });
+        });
+    });
+    mainWindow.setAlwaysOnTop(true);
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+    setInterval(followCursor, 10);
+
+
+    // Création du sous-menu pour les vues spécifiques
+    const intelligenceSubMenu = menuOptions.map(option => ({
+        label: option.label,
+        click: () => loadView(option.view)
+    }));
+    // Menu principal
+    const mainMenu = Menu.buildFromTemplate([
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'togglefullscreen' },
+                { role: 'toggledevtools' },
+                { role: 'quit' },
+                { type: "separator" },
+                { label: "Undo", accelerator: "CmdOrCtrl+Z", role: "undo" },
+                { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", role: "redo" },
+                { type: "separator" },
+                { label: "Cut", accelerator: "CmdOrCtrl+X", role: "cut" },
+                { label: "Copy", accelerator: "CmdOrCtrl+C", role: "copy" },
+                { label: "Paste", accelerator: "CmdOrCtrl+V", role: "paste" },
+                { label: "Select All", accelerator: "CmdOrCtrl+A", role: "selectAll" }
+            ]
+        },
+
+        {
+            label: 'Intelligence',
+            submenu: intelligenceSubMenu
+        }
+    ]);
+    Menu.setApplicationMenu(mainMenu);
+}
+
+function followCursor() {
+    if (isFollowingCursor) {
+        const { x, y } = screen.getCursorScreenPoint();
+        mainWindow.setPosition(x + 10, y + 10);
+    }
+}
+
+function loadView(viewName) {
+    if (views[viewName]) {
+        mainWindow.setBrowserView(views[viewName]);
+        mainWindow.on('resize', () => {
+            const { width, height } = mainWindow.getContentBounds();
+            views[viewName].setBounds({ x: 0, y: 0, width, height });
+        });
+        const { width, height } = mainWindow.getContentBounds();
+        views[viewName].setBounds({ x: 0, y: 0, width, height });
+    }
+}
+
+
+
+
+
+app.on('ready', () => {
+    createWindow();
+    globalShortcut.register('Ctrl+Space', () => {
+        isFollowingCursor = !isFollowingCursor;
+        if (mainWindow.isAlwaysOnTop()) {
+            mainWindow.setAlwaysOnTop(false);
+        } else {
+            mainWindow.setAlwaysOnTop(true);
+        }
+    });
+
+});
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
